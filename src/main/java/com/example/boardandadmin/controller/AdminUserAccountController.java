77e62cdd03ee725d927bf047a79b0915d568f289
leedg92 <49174093+leedg92@users.noreply.github.com>
2023-04-17 15:52:11 +0900

    컨트롤러 생성 및 대응되는 테스트 추가

---
+++ src/main/java/com/example/boardandadmin/controller/AdminUserAccountController.java
@@ -0,0 +1,9 @@
+package com.example.boardandadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin/members")
+@Controller
+public class AdminUserAccountController {
+}
---
+++ src/main/java/com/example/boardandadmin/controller/UserAccountManagementController.java
@@ -0,0 +1,9 @@
+package com.example.boardandadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/management/user-accounts")
+@Controller
+public class UserAccountManagementController {
+}
